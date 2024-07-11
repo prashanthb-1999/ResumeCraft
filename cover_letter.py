41744a4ec1f3f539a8b731fc8f48eb59e29ac0c1
Prashanth Bandlamudi <pbandlam@buffalo.edu>
2024-07-11 12:07:26 +0300

    Update cover_letter.py

--- cover_letter.py
+++ cover_letter.py
@@ -1,40 +1,30 @@
-# Import necessary packages
 from ibm_watson_machine_learning.foundation_models import Model
 import gradio as gr
 
 
-# Model and project settings
-model_id = "meta-llama/llama-2-70b-chat"  # Directly specifying the LLAMA2 model
+model_id = "meta-llama/llama-2-70b-chat"  
 
-# Set credentials to use the model
 my_credentials = {
     "url": "https://us-south.ml.cloud.ibm.com"
 }
 
-# Generation parameters
 gen_parms = {
-    "max_new_tokens": 512,  # Adjust as needed for the length of the cover letter
-    "temperature": 0.7  # Adjust for creativity
+    "max_new_tokens": 512,
+    "temperature": 0.7 
 }
 project_id = "skills-network"
 space_id = None
 verify = False
 
-# Initialize the model
 model = Model(model_id, my_credentials, gen_parms, project_id, space_id, verify)
 
-# Function to generate a customized cover letter
 def generate_cover_letter(company_name, position_name, job_description, resume_content):
-    # Craft the prompt for the model to generate a cover letter
     prompt = f"Generate a customized cover letter using the company name: {company_name}, the position applied for: {position_name}, and the job description: {job_description}. Ensure the cover letter highlights my qualifications and experience as detailed in the resume content: {resume_content}. Adapt the content carefully to avoid including experiences not present in my resume but mentioned in the job description. The goal is to emphasize the alignment between my existing skills and the requirements of the role."
 
     generated_response = model.generate(prompt, gen_parms)
-
-    # Extract the generated text
     cover_letter = generated_response["results"][0]["generated_text"]
     return cover_letter
 
-# Create Gradio interface for the cover letter generation application
 cover_letter_app = gr.Interface(
     fn=generate_cover_letter,
     allow_flagging="never", # Deactivate the flag function in gradio as it is not needed.
@@ -49,5 +39,4 @@ cover_letter_app = gr.Interface(
     description="Generate a customized cover letter by entering the company name, position name, job description and your resume."
 )
 
-# Launch the application
 cover_letter_app.launch()
